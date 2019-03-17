ffceedb2b17f226087ccd2fc729ebc19ff12a104
Felix Frank <felix.frank@alumni.tu-berlin.de>
2019-03-17 02:33:38 +0100

    accept json input in receive mode

--- lib/puppet/util/resource_piping.rb
+++ lib/puppet/util/resource_piping.rb
@@ -1,12 +1,18 @@
+require 'json'
 
 module Puppet
   module Util
     module ResourcePiping
 
       def self.apply_resource(input_line, face)
-        ( type, name, yaml ) = input_line.split(/ /,3)
         output = { 'failed' => true, 'changed' => false, 'error' => true, 'exception' => nil }
         begin
+          if input_line[0] == "{"
+            input = JSON.load(input_line)
+            ( type, name, yaml ) = [ input["type"], input["title"], input["params"] ]
+          else
+            ( type, name, yaml ) = input_line.split(/ /,3)
+          end
           result = face.save(type, name, yaml)
           resource = result[0]
           report = result[1]
@@ -18,6 +24,9 @@ module Puppet
             'error' => false,
             'exception' => nil,
           }
+        rescue JSON::ParserError => e
+          Puppet.err "(Applying JSON #{input_line}) #{e.message}"
+          output['exception'] = e.message
         rescue Puppet::Error => e
           Puppet.err "(Applying #{type.capitalize}[#{name}]) #{e.message}"
           output['exception'] = e.message
--- spec/integration/resource_piping_spec.rb
+++ spec/integration/resource_piping_spec.rb
@@ -12,18 +12,29 @@ describe "Puppet::Util::ResourcePiping" do
         "input"       => "file /a {}", },
       { "description" => "a resource with parameters",
         "input"       => "notify spec { message: This is a test message }", },
-      { "description" => "a resource with unknown parameters",
-        "input"       => "notify spec { adjunctive: froopily }", },
       { "description" => "a resource of unknown type",
         "input"       => "starship enterprise {}", },
+    ].each do |test|
+      it "returns a hash and no error when input is #{test["description"]}" do
+        output = subject.apply_resource(test["input"], face)
+        expect(output).to be_a Hash
+        expect(output["error"]).to be == false
+        expect(output["exception"]).to be == nil
+      end
+    end
+
+    [ { "description" => "a resource with unknown parameters",
+        "input"       => "notify spec { adjunctive: froopily }", },
       { "description" => "broken YAML for parameters",
         "input"       => "file /a { this: will: not: parse }", },
       { "description" => "a garbled input line",
         "input"       => "file /a /b /x what { ensure: file }", },
     ].each do |test|
-      it "returns a hash when given #{test["description"]}" do
+      it "returns a hash with an error when given #{test["description"]}" do
         output = subject.apply_resource(test["input"], face)
         expect(output).to be_a Hash
+        expect(output["error"]).to be == true
+        expect(output["exception"]).to_not be == nil
       end
     end
 
@@ -35,6 +46,40 @@ describe "Puppet::Util::ResourcePiping" do
     it "respects the noop parameter" do
       Puppet.expects(:warning).never
       subject.apply_resource("notify spec { loglevel: warning, noop: True }", face)
+    end
+
+    context "when given a line of JSON input" do
+
+      [ { "description" => "a plain resource",
+          "input"       => '{"type":"file", "title": "/a", "params": "{}"}', },
+        { "description" => "a resource with parameters",
+          "input"       => '{"type":"notify", "title": "spec", "params": "{message: This is a test message}"}', },
+        { "description" => "a resource of unknown type",
+          "input"       => '{"type":"starship", "title": "enterprise", "params": "{}"}', },
+      ].each do |test|
+        it "returns a hash and no error when input is #{test["description"]}" do
+          output = subject.apply_resource(test["input"], face)
+          expect(output).to be_a Hash
+          expect(output["error"]).to be == false
+          expect(output["exception"]).to be == nil
+        end
+      end
+
+      [ { "description" => "a resource with unknown parameters",
+          "input"       => '{"type":"notify", "title": "spec", "params": "{adjunctive: froopily}"}', },
+        { "description" => "broken YAML for parameters",
+          "input"       => '{"type":"file", "title": "/a", "params": "{this: will: not: parse}"}', },
+        { "description" => "a garbled input line",
+          "input"       => '{"type":"file", "title": "/a": "/b": "/x", "what": "{ ensure: file }"}', },
+      ].each do |test|
+        it "returns a hash with an error when input is #{test["description"]}" do
+          output = subject.apply_resource(test["input"], face)
+          expect(output).to be_a Hash
+          expect(output["error"]).to be == true
+          expect(output["exception"]).to_not be == nil
+        end
+      end
+
     end
 
   end
